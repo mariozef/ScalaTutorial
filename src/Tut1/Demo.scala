1a6295576b4eb896b0cddb7968ef2bd1ba25a629
mariozef <zefirr@o2.pl>
2016-11-11 16:04:22 +0100

    Drugi obiekt

---
+++ src/Tut1/Demo.scala
@@ -0,0 +1,16 @@
+package Tut1
+
+/**
+  * Created by mariozef on 2016-11-11.
+  */
+object Demo {
+  def main(args: Array[String]): Unit = {
+    var myVar :Int = 10;
+    val myVal :String = "Hello Scala with datatype declaration.";
+    var myVar1 = 20;
+    val myVal1 = "Hello Scala new without datatype declaration.";
+
+    println(myVar); println(myVal); println(myVar1);
+    println(myVal1);
+  }
+}

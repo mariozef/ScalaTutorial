d42f113c26a41375b90df784f04a7cee8f36fe34
mariozef <zefirr@o2.pl>
2016-11-11 16:13:02 +0100

    Dziedziczenie

---
+++ src/Tut2/Location.scala
@@ -0,0 +1,17 @@
+package Tut2
+
+/**
+  * Created by mariozef on 2016-11-11.
+  */
+class Location(override val xc: Int, override val yc: Int, zc: Int) extends Point(xc, yc) {
+  var z: Int = zc
+
+  def move(dx: Int, dy: Int, dz: Int) {
+    x = x + dx
+    y = y + dy
+    z = z + dz
+    println ("Point x location : " + x);
+    println ("Point y location : " + y);
+    println ("Point z location : " + z);
+  }
+}

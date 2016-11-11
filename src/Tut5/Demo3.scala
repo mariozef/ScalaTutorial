3ddc83a125d6980ecaf5f95a925fbb15bdc534b7
mariozef <zefirr@o2.pl>
2016-11-11 16:35:23 +0100

    Concatenate Arrays

---
+++ src/Tut5/Demo3.scala
@@ -0,0 +1,20 @@
+package Tut5
+
+import Array._
+
+/**
+  * Created by mariozef on 2016-11-11.
+  */
+object Demo3 {
+  def main(args: Array[String]) {
+    var myList1 = Array(1.9, 2.9, 3.4, 3.5)
+    var myList2 = Array(8.9, 7.9, 0.4, 1.5)
+
+    var myList3 =  concat( myList1, myList2)
+
+    // Print all the array elements
+    for ( x <- myList3 ) {
+      println( x )
+    }
+  }
+}

7e0dec3f76697b591d6a8f4ecbcc8f1711aadbb2
mariozef <zefirr@o2.pl>
2016-11-11 16:13:49 +0100

    Dziedziczenie

---
+++ src/Tut2/Demo2.scala
@@ -0,0 +1,13 @@
+package Tut2
+
+/**
+  * Created by mariozef on 2016-11-11.
+  */
+object Demo2 {
+  def main(args: Array[String]) {
+    val loc = new Location(10, 20, 15);
+
+    // Move to a new location
+    loc.move(10, 10, 5);
+  }
+}

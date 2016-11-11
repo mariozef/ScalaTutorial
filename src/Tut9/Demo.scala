50d57476297d42aa34d833714d16d493aa8d660a
mariozef <zefirr@o2.pl>
2016-11-11 16:49:29 +0100

    Reading a Line from Command Line

---
+++ src/Tut9/Demo.scala
@@ -0,0 +1,13 @@
+package Tut9
+
+/**
+  * Created by mariozef on 2016-11-11.
+  */
+object Demo {
+  def main(args: Array[String]) {
+    print("Please enter your input : " )
+    val line = Console.readLine
+
+    println("Thanks, you just typed: " + line)
+  }
+}

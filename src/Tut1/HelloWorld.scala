8d4a6aee20f7636b5938c25b88563a309d2191d5
mariozef <zefirr@o2.pl>
2016-11-11 15:58:23 +0100

    Dodano ciało obiektu

--- src/Tut1/HelloWorld.scala
+++ src/Tut1/HelloWorld.scala
@@ -1,8 +1,14 @@
 package Tut1
 
 /**
-  * Created by MGolus on 2016-11-11.
+  * https://www.tutorialspoint.com/scala/scala_basic_syntax.htm
+  * Created by mariozef on 2016-11-11.
   */
 object HelloWorld {
-
+  /* This is my first java program.
+     * This will print 'Hello World' as the output
+     */
+  def main(args: Array[String]): Unit = {
+    println("Hello world!") // prints Hello World
+  }
 }
